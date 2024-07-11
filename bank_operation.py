594088afbced41722177d7a3e8ddf6f5a5cfeec7
nagariyaarchit <nagariyaarchit@gmail.com>
2024-07-10 20:46:59 -0700

    adding multiple bank accounts

--- bank_operation.py
+++ bank_operation.py
@@ -6,6 +6,7 @@ class bank:
         self.balance += money_to_deposit
     def withdrawal(self, money_to_withdraw):
         if self.balance < money_to_withdraw:
+            print("")
             print("Transaction has been cancelled because there is not enough money for you to withdraw")
             print("")
         else:
@@ -14,12 +15,16 @@ class bank:
         return self.balance
 
 a = bank("Archit",0)
+name = "Archit"
+bank_account_list = []
+bank_account_list.append(name)
 
 while True:
     print("If you wish to check your bank balance press 1")
     print("If you wish to deposit money press 2")
     print("if you wish to withdraw money press 3")
     print("If you wish to quit press 4")
+    print("If you wish to open a bank account press 5")
     print("")
 
     choice = int(input("Please pick an option:"))
@@ -36,3 +41,7 @@ while True:
         z = a.withdrawal(money_to_withdraw)
     if choice == 4:
         break
+    if choice == 5:
+        new_bank_account = input("Enter the new bank account name")
+        x = str(new_bank_account)
+        bank_account_list.append(x)
